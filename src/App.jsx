f442666db0cc425fca26a308e974d16d8952b256
rawfid <rawfid.shuvo@outlook.com>
2026-01-29 00:58:37 +0100

    config

--- src/App.jsx
+++ src/App.jsx
@@ -62,11 +62,11 @@ import {
 // Uses environment variables for Firebase Config in Canvas
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "game-hub-ff8aa.firebaseapp.com",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: "game-hub-ff8aa.firebasestorage.app",
-  messagingSenderId: "586559578902",
-  appId: "1:586559578902:web:c0d92602ad20ed876aa637"
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
